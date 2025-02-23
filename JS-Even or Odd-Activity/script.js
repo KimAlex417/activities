3c3d1bb2a84e32dfb79ded337b18296ef26e6a28
KimAlex417 <kimalexyebra417@gmail.com>
2025-02-23 23:13:44 +0800

    JS-Even or Odd-Activity
    Added Day 4 - Loops & Conditionals Activity

---
+++ JS-Even or Odd-Activity/script.js
@@ -0,0 +1,14 @@
+let number = prompt("Enter a number:");
+number = parseInt(number);
+
+if (number % 2 === 0) {
+    alert(number + " is an even number.");
+} else {
+    alert(number + " is an odd number.");
+}
+
+console.log("Numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
